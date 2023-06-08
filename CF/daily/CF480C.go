4e558045fef6eeb2a7c60c373fc1d6f31e3dfdab
Marchons <d1286360646@outlook.com>
2023-06-08 15:06:43 +0800

    add a CF480C which idk how to get it

---
+++ CF/daily/CF480C.go
@@ -0,0 +1,32 @@
+package daily
+
+import (
+	. "fmt"
+	"io"
+)
+
+func CF480C(in io.Reader, out io.Writer) {
+	const mod = 1_000_000_007
+	var n, a, b, k int
+	Fscan(in, &n, &a, &b, &k)
+	if a > b {
+		a = n + 1 - a
+		b = n + 1 - b
+	}
+	f := make([]int, b)
+	f[a] = 1
+	s := make([]int, b+1)
+	for ; k > 0; k-- {
+		for i, v := range f {
+			s[i+1] = (s[i] + v) % mod
+		}
+		for y := 1; y < b; y++ {
+			f[y] = (s[y+(b-y-1)/2+1] - f[y]) % mod
+		}
+	}
+	ans := int64(0)
+	for _, v := range f {
+		ans += int64(v)
+	}
+	Fprint(out, (ans%mod+mod)%mod)
+}
--- main.go
+++ main.go
@@ -1,5 +1,11 @@
 package main
 
-func main() {
+import (
+	"go_algorithm/CF/daily"
 
+	"os"
+)
+
+func main() {
+	daily.CF480C(os.Stdin, os.Stdout)
 }
